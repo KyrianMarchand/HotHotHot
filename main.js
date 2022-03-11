33c7cf9cf3bfd260e94eca912432b0ef1914c100
Thibaud Melano <72598187+ThibaudMelano@users.noreply.github.com>
2022-03-11 11:57:14 +0100

    implémentation du lot3 => PWA Fonctionnel + tentative de navbar

---
+++ main.js
@@ -0,0 +1,62 @@
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker
+  
+      .register('sw.js') // à adapter à l'URL du projet
+  
+      .then(() => { console.log('Service Worker Registered'); });
+  
+  }
+  
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.getElementsByClassName.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+  });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclcick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+  
